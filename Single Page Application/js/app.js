efa2d282b499f71412bb1d453c5751bebea99e13
Prashant <prashant.p@simformsolutions.com>
2023-02-16 18:35:57 +0530

    UI Changed and Added Scroll effect

---
+++ Single Page Application/js/app.js
@@ -0,0 +1,37 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 4
+        }
+    }
+})
+
+const scrolltoSection = (ele) => { ele.scrollIntoView({ behavior: 'smooth' }); }
+
+
+const backToTopBtn = document.getElementById("back-to-top-btn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        backToTopBtn.style.display = "block";
+    } else {
+        backToTopBtn.style.display = "none";
+    }
+};
+
+// When the user clicks on the button, scroll to the top of the document
+backToTopBtn.addEventListener("click", function () {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
