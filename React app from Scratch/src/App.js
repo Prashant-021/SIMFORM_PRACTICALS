612e82084e568fd79e8132719e13d63cdfba7fe4
Prashant <prashant.p@simformsolutions.com>
2023-04-10 16:14:13 +0530

    Development and production build

--- React app from Scratch/src/App.js
+++ React app from Scratch/src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
+import 'app.css'
+import tshirt from './tshirt.jpg';
+
+import Component1 from './components/Component1';
 
 const App = () => (
     <div>
-        <h1>Hello React!!</h1>
+        <h1>Hello React!!!</h1>
         <h2>React app from scratch</h2>
+        <Component1 />
+        <img src={tshirt} alt="" width={400}/>
     </div>
 )
 export default App
