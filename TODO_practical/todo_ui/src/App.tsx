d31b7d83854c4cc9725f8915b70baf847a0025e4
Prashant <prashant.p@simformsolutions.com>
2023-04-17 10:28:44 +0530

    Files Added

---
+++ TODO_practical/todo_ui/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Header from './components/header/Header';
+import Bodysec from './components/bodysection/Bodysec';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Bodysec />
+    </div>
+  );
+}
+
+export default App;
