12c5b42685d00ef7679f1176f8bbaea85004e56b
Prashant <prashant.p@simformsolutions.com>
2023-05-15 16:58:47 +0530

    Header and Footer Added

--- user-management/src/components/Dashboard/Dashboard.tsx
+++ user-management/src/components/Dashboard/Dashboard.tsx
@@ -1,12 +1,13 @@
 import React, { useLayoutEffect } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom';
 import { User } from '../../interface';
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
 
 
 const Dashboard = () => {
     const location = useLocation();
     const user: User | undefined = location.state && location.state.user;
-    console.log(user);
     const Navigate = useNavigate()
     const handleSubmit = () => {
         Navigate('/Login')
@@ -20,33 +21,35 @@ const Dashboard = () => {
     }, [Navigate])
 
     return (
-        user ? (<div className='lg:flex w-full'>
-            <div className="lg:w-1/5 h-[50vh] lg:h-[100vh] bg-[#005ae6] flex items-center drop-shadow-2xl relative">
-                <div className="overflow-hidden flex items-center justify-center lg:absolute rounded-full bg-yellow-300 h-80 w-80 right-auto bottom-0 lg:bottom-auto lg:-right-40 ">
-                    <img src={user.profilepicture as string} alt="" className='h-[100%] w-[100%]' />
+        user ? (<div className='w-full '>
+            <Header profileImg={user.profilepicture as string}/>
+            <div className='lg:flex '>
+                <div className="lg:w-1/5 h-[50vh] lg:h-[89vh] bg-[#005ae6] flex items-center justify-center relative">
+                    <div className="overflow-hidden flex items-center justify-center lg:absolute rounded-full  drop-shadow-2xl h-80 w-80 right-auto bottom-0 lg:bottom-auto lg:-right-40 ">
+                        <img src={user.profilepicture as string} alt="" className='h-[100%] w-[100%]' />
+                    </div>
+                </div>
+                <div className="lg:w-4/5 h-[50vh] lg:h-[89vh] flex flex-col items-center justify-center">
+                    <div className='border-transparent drop-shadow-2xl border-2 p-10 w-[50%] rounded-xl bg-white'>
+                        <div className="heading mb-9">
+                            <h1 className='text-black-700 text-4xl underline underline-offset-8 decoration-8 decoration-blue-400 dark:decoration-blue-600'>Information</h1>
+                            <br />
+                        </div>
+                        <div className="my-4">
+                            <h1 className='text-1xl mb-2'><span className='font-bold'>Name:  </span> {user.name}</h1>
+                            <h1 className='text-1xl mb-2'><span className='font-bold'>Email: </span> {user.email}</h1>
+                            <h1 className='text-1xl mb-2'><span className='font-bold'>Phone: </span> {user.phone}</h1>
+                        </div>
+                        <button className='py-2 mt-7 float-right px-8 bg-[#005ae6] rounded-md text-white hover:bg-black ' onClick={handleSubmit}>Logout</button>
+                    </div>
                 </div>
             </div>
-            <div className="lg:w-4/5 h-[50vh] lg:h-[100vh] flex flex-col items-center justify-center">
-                <div className='border-transparent drop-shadow-2xl border-2 p-10 rounded-xl bg-white'>
-                    <div className="heading mb-9">
-                        <h1 className='text-blue-700 text-4xl'>Information</h1>
-                        <br />
-                    </div>
-                    <div className="my-4">
-                        <h1 className='text-2xl mb-2'><span className='font-bold'>Name: </span> {user.name}</h1>
-                        <h1 className='text-2xl mb-2'><span className='font-bold'>Email: </span> {user.email}</h1>
-                        <h1 className='text-2xl mb-2'><span className='font-bold'>Phone: </span> {user.phone}</h1>
-                    </div>
-                    <button className='py-2 mt-7 float-right px-8 bg-[#005ae6] rounded-md text-white hover:bg-black ' onClick={handleSubmit}>Logout</button>
-                </div>
-            </div>
+            <Footer/>
         </div>) :
             <div>No user Avaliable</div>
+
+
+
     )
 }
-// const mapStateToProps = (state: RootState) => ({
-//     user: getUser(state),
-// });
-
-// export default connect(mapStateToProps)(Dashboard);
 export default Dashboard;
---
+++ user-management/src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const Footer = (props: Props) => {
+    return (
+        <footer className='footer text-center py-2  bottom-0 w-full bg-white'>
+            <a href="https://github.com/Prashant-021/SIMFORM_PRACTICALS/tree/user-management/user-management">Link to GitHub Repository</a>
+        </footer>
+    )
+}
+
+export default Footer
